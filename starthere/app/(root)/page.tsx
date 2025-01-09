7105c868baed5e32b511f7363672c5fb97225d5e
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2025-01-09 14:02:53 +0530

    search form added

--- starthere/app/(root)/page.tsx
+++ starthere/app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams  }: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -14,6 +21,7 @@ export default function Home() {
         </p>
       </section>
 
+      <SearchForm query={query}/>
     </>
   );
 }
