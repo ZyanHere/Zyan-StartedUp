e98980370ae2fa28b3f90a7be1deea42047569bb
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2025-01-09 14:30:03 +0530

    startup card added

--- starthere/app/(root)/page.tsx
+++ starthere/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams  }: {
@@ -6,6 +7,16 @@ export default async function Home({
 }) {
 
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: 'Yesterday',
+    views: 55,
+    author: { _id:1, name: 'Zyan'},
+    description: "this is description",
+    image: "https://media.gettyimages.com/id/1385970223/photo/great-idea-of-a-marketing-strategy-plan-at-a-creative-office.jpg?s=612x612&w=gi&k=20&c=6eNZA8XGYgYJmD1gS7oWmGxFF6BvmWtozndUAHkpF7M=",
+    category: 'Robots',
+    title: "We Robots"
+  }]
 
   return (
     <>
@@ -20,6 +31,23 @@ export default async function Home({
           Competitions.
         </p>
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+
 
       <SearchForm query={query}/>
     </>
