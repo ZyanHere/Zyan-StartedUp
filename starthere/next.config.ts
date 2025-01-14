2305ed18e648240b892cd864eb21cffced9e9d0c
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2025-01-14 14:46:36 +0530

    last updates

--- starthere/next.config.ts
+++ starthere/next.config.ts
@@ -17,9 +17,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    ppr: "incremental",
-  },
+  // experimental: {
+  //   ppr: "incremental",
+  // },
   devIndicators: {
     appIsrStatus: true,
     buildActivity: true,
